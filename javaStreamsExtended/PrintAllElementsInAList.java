6114f422a1a7c45641bcb265d04503aa53c646f7
shreyatulsyan01 <shreyatulsyan01@gmail.com>
2025-11-20 23:47:10 +0530

    More coding for Lambda

--- javaStreamsExtended/PrintAllElementsInAList.java
+++ javaStreamsExtended/PrintAllElementsInAList.java
@@ -1,6 +1,6 @@
 package javaStreamsExtended;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -27,6 +27,14 @@ class CheckIfStringStartsWithA {
     public static void main(String args[]) {
         Predicate<String> f = n -> n.startsWith("A");
         System.out.println(f.test("Ana"));
+    }
+}
+//nhi aya
+class FindfirststringstartingwithA{
+    public static void main(String args[]) {
+        List<String> list = Arrays.asList("Ravi", "Amit", "Raj");
+        Optional<String> res = list.stream().filter(n->n.startsWith("A")).findFirst();
+        System.out.println(res);
     }
 }
 class CountStringsWithLengthGreaterThan5 {
@@ -73,3 +81,107 @@ class SumOfNumbersUsingReduce {
         System.out.println(sum);
     }
 }
+class Printlengthsofeachstringinalist{
+    public static void main(String args[]){
+        List<String> countries = Arrays.asList("India","USA", "Vietnam");
+        Function<String,Integer> f  = n -> n.length();
+        countries.stream().map(f).forEach(System.out::println);
+    }
+}
+class Printlengthsofeachstringinalist2{
+    public static void main(String args[]){
+        List<String> countries = Arrays.asList("India","USA", "Vietnam");
+        countries.forEach(n -> System.out.println(n.length()));
+
+        //Sort a list of strings using lambda
+        List<String> items = Arrays.asList("Banana", "Apple", "Mango");
+        items.sort((a, b) -> a.compareTo(b));
+
+    }
+}
+class Convertlistofstringstouppercase{
+    public static void main(String args[]) {
+        List<String> list = Arrays.asList("java", "lambda", "code");
+        list.stream().map(n -> n.toUpperCase()).forEach(n -> System.out.println(n));
+    }
+}
+class Sumallnumbersinalist{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        //Using reduce
+        int sum = list.stream().reduce(0,(a,b)->a+b);
+        System.out.println(sum);
+    }
+}
+class FindMaximumNumberUsingLambda{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        int sum = list.stream().max((a,b) -> a.compareTo(b)).get();
+        System.out.println(sum);
+    }
+}
+class Countelementsgreaterthan50{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(30,60,90,80);
+        long sum = list.stream().filter(n->n>80).count();
+        System.out.println(sum);
+    }
+}
+class RemoveDuplicatesusinglambda{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(30,60,30,90,80);
+        list.stream().distinct().forEach(n -> System.out.println(n));
+    }
+}
+class Printsquareofeachnumber{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        list.stream().map(n->n*n).forEach(n -> System.out.println(n));
+    }
+}
+class Convertlistofintegerstolistofstrings{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        list.stream().map(n->"Num" + n).forEach(n -> System.out.println(n));
+    }
+}
+
+//Create your own functional interface and use lambda
+@FunctionalInterface
+interface ShreyasCustomizedInterface<T>{
+    void doSomething(T t);
+}
+
+class ClassForUsingLambdaForCustomizedInterface{
+    public static void main(String args[]) {
+    List<String> list = Arrays.asList("Red","Blue","Orange");
+    ShreyasCustomizedInterface s = (o) -> System.out.println(o);
+    s.doSomething(list);
+    }
+}
+//nhi aya
+class Iterateamapusinglambda{
+    public static void main(String args[]) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("S",2);
+        map.put("A",4);
+        map.forEach((k,v) -> System.out.println(k + " -> " + v));
+    }
+}
+//nhi aya
+class SortListInDescendingOrder{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        list.sort((a, b) -> b - a);
+        System.out.println(list);
+    }
+}
+class CheckIfAnyNumberIsDivisibleBy5{
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        boolean res = list.stream().anyMatch(n->n%5==0);
+        System.out.println(res);
+    }
+}
+
